8208db7ecf4ebaecc8fcdc09f7daa287183215af
CarolineT9 <caroltvrs@gmail.com>
2024-05-29 14:54:52 -0300

    add basic examples With Option API and Composition API

---
+++ vuejs-ecommerce/src/exemples/ContadorComposition.vue
@@ -0,0 +1,24 @@
+<!--Composition API-->
+<script setup lang="ts"> 
+    import {onMounted, ref} from 'vue'
+    const count = ref (0);
+    
+
+    function increase( ){
+        return count.value += 1
+    }
+    function decrease(){
+        return count.value -= 1
+    }
+
+    onMounted(function () {
+        console.log('O valor inicial do contador é ' + ' ' + count.value )
+    })
+</script>
+<template>
+  <p>Olá mundo! Sou a versão com compostion API </p>
+  <h1>Contador : {{ count }}</h1>
+
+  <button @click="increase">Incrementar contador</button>
+  <button @click="decrease">Decrementar contador</button>
+</template>
---
+++ vuejs-ecommerce/src/exemples/ContadorOptions.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Contador: {{ count }}</h1>
+  <button @click="plusCount">Aumentar contador</button>
+  <button @click="decreaseCount">Diminuir o contador</button>
+</template>
+
+<script lang="ts">
+export default {
+    data (){
+        return {
+            count:0
+        }
+    },
+    methods: {
+        plusCount(){
+            return this.count = this.count + 1
+        },
+        decreaseCount(){
+            return this.count -= 1
+        }
+    },
+    mounted() {
+        console.log('O valor inicial do contador é' + ' ' + this.count)
+    },
+}
+</script>
+
+<style>
+
+</style>
